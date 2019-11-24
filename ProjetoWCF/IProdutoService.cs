69ec2615b9de27f59176ec98f965f3cefa27dd8f
guilhermesabreu <guilhermeabreucc@gmail.com>
2019-11-24 00:38:32 -0300

    Aplicação Completa

---
+++ ProjetoWCF/IProdutoService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Xml;
+
+namespace ProjetoWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProdutoService" in both code and config file together.
+    [ServiceContract]
+    public interface IProdutoService
+    {
+        [OperationContract]
+        [ReferencePreservingDataContractFormat]
+        List<Produto> FindAll();
+
+        [OperationContract]
+        [ReferencePreservingDataContractFormat]
+        Produto Find(int id);
+
+        [OperationContract]
+        Produto New(Produto produto);
+
+        [OperationContract]
+        Produto Update(Produto produto);
+
+        [OperationContract]
+        Produto DeleteId(int id);
+
+        [OperationContract]
+        Produto Delete(Produto produto);
+
+    }
+
+    public class ReferencePreservingDataContractFormatAttribute
+       : Attribute, IOperationBehavior
+    {
+        #region IOperationBehavior Members
+        public void AddBindingParameters(OperationDescription description,
+            BindingParameterCollection parameters)
+        {
+        }
+
+        public void ApplyClientBehavior(OperationDescription description,
+            ClientOperation proxy)
+        {
+            IOperationBehavior innerBehavior =
+              new ReferencePreservingDataContractSerializerOperationBehavior(description);
+            innerBehavior.ApplyClientBehavior(description, proxy);
+        }
+
+        public void ApplyDispatchBehavior(OperationDescription description,
+            DispatchOperation dispatch)
+        {
+            IOperationBehavior innerBehavior =
+              new ReferencePreservingDataContractSerializerOperationBehavior(description);
+            innerBehavior.ApplyDispatchBehavior(description, dispatch);
+        }
+
+        public void Validate(OperationDescription description)
+        {
+        }
+
+        #endregion
+    }
+
+
+
+    public class ReferencePreservingDataContractSerializerOperationBehavior :
+        DataContractSerializerOperationBehavior
+    {
+        #region Ctor
+        public ReferencePreservingDataContractSerializerOperationBehavior(
+            OperationDescription operationDescription)
+            : base(operationDescription) { }
+        #endregion
+
+        #region Public Methods
+
+        public override XmlObjectSerializer CreateSerializer(Type type,
+               XmlDictionaryString name, XmlDictionaryString ns,
+               IList<Type> knownTypes)
+        {
+            return new DataContractSerializer(type, name, ns, knownTypes,
+                2147483646 /*maxItemsInObjectGraph*/,
+                false/*ignoreExtensionDataObject*/,
+                true/*preserveObjectReferences*/,
+                null/*dataContractSurrogate*/);
+        }
+        #endregion
+    }
+
+}
---
+++ ProjetoWCF/ProdutoService.svc.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjetoWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProdutoService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ProdutoService.svc or ProdutoService.svc.cs at the Solution Explorer and start debugging.
+    public class ProdutoService : IProdutoService
+    {
+
+        private ProjetoModeloDBEntities _db;
+
+        public List<Produto> FindAll()
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                List<Produto> produtos = _db.Produto.Include("Cliente").ToList();
+                return produtos;
+            }
+        }
+
+        public Produto Find(int id)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                Produto produto = _db.Produto.Single(x => x.ProdutoId.Equals(id));
+                return produto;
+            }
+        }
+
+        public Produto New(Produto produto)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Produto.Add(produto);
+                _db.SaveChanges();
+                return produto;
+            }
+        }
+
+        public Produto Update(Produto produto)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Entry(produto).State = EntityState.Modified;
+                _db.SaveChanges();
+                return produto;
+            }
+        }
+
+        public Produto DeleteId(int id)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                Produto produto = _db.Set<Produto>().Find(id);
+                _db.Set<Produto>().Remove(produto);
+                _db.SaveChanges();
+                return produto;
+            }
+        }
+
+        public Produto Delete(Produto produto)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Set<Produto>().Remove(produto);
+                _db.SaveChanges();
+                return produto;
+            }
+        }
+    }
+}
